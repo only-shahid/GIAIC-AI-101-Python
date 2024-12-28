1523583e82e44732873bb2b01550031550e825a1
Muhammad Shahid <143910158+only-shahid@users.noreply.github.com>
2024-12-28 11:43:14 +0500

    Class Practice File

---
+++ app.py
@@ -0,0 +1,85 @@
+# name: str = "Sir Aneeq"
+# age: int = 20
+# is_active: bool = True
+
+# yahan men variable ko override kr rha hn
+# name = "Sir Ameen"
+
+# print(name)
+
+
+# 1. Arithematic operators  
+# 2. Assigment operators
+# 3. Comparison operators (boolean) True/False
+# 4. Logical operators
+
+
+#  1. Arithematic operators  
+
+#  7 + 5 = 12
+print(7 + 5)
+print(10 - 5)
+print(10 / 2)
+#     5 X 2
+print(5 * 2)  
+# exponent  8^2 = 8 X 8
+print(8 ** 2)  
+# floor division
+print(11 // 2) 
+#      11 / 2 = 1  remainder / modulus
+print(11 % 2)
+
+num1: int = 5 
+num2: int = 5
+
+#           5   +  5
+sum: int = num1 + num2
+#           5   -  5
+sub: int = num1 - num2
+#           5   /  5
+div: int = num1 / num2
+#           5   x  5
+mul: int = num1 * num2
+print(mul)
+
+
+# 2. Assigment operators
+
+x: int = 15
+#  15 + 5 = 15
+x = x + 5
+# x = x - 5
+# x = x * 2
+
+# x: int = 15
+#   15 * 2 = 30
+# remainder / modulus
+# x = x % 3 
+
+print(x)
+
+
+# 3. Comparison operators (True/False)
+#  ==   (equal to) 
+#  >    (greater than)
+#  <    (less than)
+#  >=   (greater or equal to)
+#  <=    (less than or equal to)
+#  !=    (not equal to)
+
+#  mera pehla operand/value  
+# dusre wale operand/value k baraber he
+
+num1: int = 10 
+num2: int = 11
+# print(10 == 10)
+print(num1 == num2)
+print(num2 > num1)
+print(num2 < num1)
+print(num1 >= num2 )
+print(num1 <= num2)
+print(num1 != num2)
+
+
+# Logical operators
+#  and, or , not
